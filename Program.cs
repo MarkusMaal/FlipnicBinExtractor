269b546ee59b66e7a66b08b9561f34ece32822dd
Markus Maal <markus.maal@gmail.com>
2022-03-29 16:01:31 +0300

    Added subfolder repacking and directory listing support. Included VGMToolBox settings, for those who are interested.

--- Program.cs
+++ Program.cs
@@ -8,22 +8,27 @@ namespace FlipnicBinExtractor
     {
         static int Main(string[] args)
         {
-            Console.WriteLine("Flipnic Game Data Extractor 0.0");
-            string me = System.AppDomain.CurrentDomain.FriendlyName;
+            Console.WriteLine("Flipnic Game Data Extractor And Repacker");
+            string me = AppDomain.CurrentDomain.FriendlyName;
 
             if (string.Join("", args).Contains("/?"))
             {
                 string help = "Help" +
                     "\n" +
-                    string.Format("\nUsage: {0} [command] [source] [destination]", me) +
+                    string.Format("\nUsage: {0} [/e /c /f] [source] [destination]", me) +
                     "\n" +
-                    "\ncommand     - Use the /e switch for extracting, /c switch for repacking" +
-                    "\nsource      - The source file. If extracting, this must be a .BIN file. If you're repacking, make sure this points to a folder" +
+                    "\n/e          - Extract data from BIN file" +
+                    "\n/l          - Display directory tree without extracting data" +
+                    "\n/c          - Create BIN file using files in a folder. Any subfolders containing files will be aliased and the A file will be interpreted as a subfolder in TOC." +
+                    "\n/f          - Create a subdirectory file, which you can use for repacking. Note that this is significantly slower, because each file is 1 byte addressed." +
+                    "\nsource      - The source file. If extracting, this must be a .BIN file. If you're repacking, make sure this points to a folder." +
                     "\ndestination - The destination folder (when extracting) or file (when repacking)" +
                     "\n" +
                     "\nExamples:" +
                     "\n\n" +
                     string.Format("\n{0} /e STR.BIN STR", me) +
+                    string.Format("\n{0} /f STR STR\\A", me) +
+                    string.Format("\n{0} /l RES.BIN", me) +
                     string.Format("\n{0} /c TUTO TUTO.BIN", me) + "\n";
                 Console.Write(help);
                 return 0;
@@ -33,7 +38,7 @@ namespace FlipnicBinExtractor
                 Console.WriteLine(string.Format("No command specified. To see all available commands, type \"{0} /?\".", me));
                 return 4;
             }
-            else if (args.Length < 3)
+            else if ((args[0].ToLower() != "/l") && (args.Length < 3))
             {
                 Console.WriteLine("Not enough arguments specified!");
                 return 5;
@@ -45,32 +50,272 @@ namespace FlipnicBinExtractor
             }
             else
             {
-                if (args[0] == "/e")
+                switch (args[0].ToLower())
                 {
-                    switch (ExtractBin(args[1], args[2]))
+                    case "/e":
+                        switch (ExtractBin(args[1], args[2]))
+                        {
+                            case 0:
+                                Console.WriteLine("Command completed successfully.");
+                                return 0;
+                            case 1:
+                                Console.WriteLine("Cannot continue. Must overwrite directory to extract!");
+                                return 1;
+                            default:
+                                Console.WriteLine("Unknown error has occoured.");
+                                return 999;
+                        }
+                    case "/c":
+                        switch (RepackBin(args[1], args[2]))
+                        {
+                            case 0:
+                                Console.WriteLine("Command completed successfully.");
+                                return 0;
+                            case 1:
+                                Console.WriteLine("Cannot continue. Must overwrite existing BIN file to repack!");
+                                return 1;
+                            default:
+                                Console.WriteLine("Unknown error has occoured.");
+                                return 999;
+                        }
+                    case "/l":
+                        switch (ListBin(args[1]))
+                        {
+                            case 0:
+                                Console.WriteLine("Command completed successfully.");
+                                return 0;
+                            case 1:
+                                Console.WriteLine("Specified file does not exist.");
+                                return 1;
+                            default:
+                                Console.WriteLine("Unknown error has occoured.");
+                                return 999;
+                        }
+                    case "/f":
+                        switch (CreateFolder(args[1], args[2]))
+                        {
+                            case 0:
+                                Console.WriteLine("Command completed successfully.");
+                                return 0;
+                            case 1:
+                                Console.WriteLine("Cannot continue. Must overwrite file to create folder");
+                                return 1;
+                            default:
+                                Console.WriteLine("Unknown error has occoured.");
+                                return 999;
+                        }
+                        
+                    default:
+                        Console.WriteLine("The syntax of the requested command is incorrect.");
+                        return 3;
+                }
+            }
+        }
+
+        static int RepackBin(string source, string destination)
+        {
+            return 0;
+        }
+
+        static int CreateFolder(string source, string destination)
+        {
+            if (File.Exists(destination))
+            {
+                Console.Write("Specified file already exists. Overwrite? [Y/N] ");
+                ConsoleKey result = Console.ReadKey().Key;
+                while (!((result == ConsoleKey.Y) || (result == ConsoleKey.N)))
+                {
+                    result = Console.ReadKey().Key;
+                }
+                if (result == ConsoleKey.Y)
+                {
+                    Console.Write("\n");
+                    File.Delete(destination);
+
+                }
+                else if (result == ConsoleKey.N)
+                {
+                    Console.Write("\n");
+                    return 1;
+                }
+            }
+            Console.WriteLine("Analyzing directory...");
+            int toc_length = 0;
+            long eof = 0;
+            Dictionary<string, long> fs_entries = new Dictionary<string, long>();
+            foreach (FileInfo fi in new DirectoryInfo(source).EnumerateFiles())
+            {
+                if (fi.Name != destination.Split('\\')[^1])
+                {
+                    fs_entries[fi.Name] = fi.Length;
+                    toc_length += 64;
+                    eof += fi.Length;
+                }
+            }
+            toc_length += 64;
+            eof += toc_length;
+            Console.WriteLine("Writing TOC data...");
+            byte[] entry = new byte[64];
+            long sum_of_bytes = 0;
+            foreach (KeyValuePair<string, long> record in fs_entries)
+            {
+                Console.WriteLine(string.Format("\t{0}, offset = {1}", record.Key, sum_of_bytes.ToString("X")));
+                List<byte> blist = new List<byte>();
+                int nulllength = 60 - record.Key.Length;
+                for (int i = 0; i < record.Key.Length; i++) {
+                    blist.Add((byte)record.Key[i]);
+                }
+                for (int i = 0; i < nulllength; i++)
+                {
+                    blist.Add(0x00);
+                }
+                // use uint to make sure we get 4 bytes (a.k.a. 32 bits)
+                blist.AddRange(BitConverter.GetBytes((uint)(toc_length + sum_of_bytes)));
+                sum_of_bytes += record.Value;
+                entry = blist.ToArray();
+                using (var stream = new FileStream(destination, FileMode.Append))
+                {
+                    stream.Write(entry, 0, entry.Length);
+                }
+            }
+            Console.WriteLine(string.Format("End Of Mem Data, offset = {0}", eof.ToString("X")));
+            string eom = "*End Of Mem Data";
+            List<byte> blist2 = new List<byte>();
+            int nulllength2 = 60 - eom.Length;
+            for (int i = 0; i < eom.Length; i++)
+            {
+                blist2.Add((byte)eom[i]);
+            }
+            for (int i = 0; i < nulllength2; i++)
+            {
+                blist2.Add(0x00);
+            }
+            blist2.AddRange(BitConverter.GetBytes((uint)eof));
+            entry = blist2.ToArray();
+            using (var stream = new FileStream(destination, FileMode.Append))
+            {
+                stream.Write(entry, 0, entry.Length);
+            }
+            Console.WriteLine("End of TOC, begin writing data...");
+            foreach (KeyValuePair<string, long> file in fs_entries)
+            {
+                Console.WriteLine(string.Format("\tWriting {0}... ({1} KiB)", file.Key, file.Value / 1024));
+                using (Stream src = File.OpenRead(source + "\\" + file.Key))
+                {
+                    byte[] buffer = new byte[1];
+                    int offset = 0;
+                    while ((offset = src.Read(buffer, 0, buffer.Length)) > 0)
                     {
-                        case 0:
-                            Console.WriteLine("Command completed successfully.");
-                            return 0;
-                        case 1:
-                            Console.WriteLine("Cannot continue. Must overwrite directory to extract!");
-                            return 1;
+                        using (var stream = new FileStream(destination, FileMode.Append))
+                        {
+                            stream.Write(buffer, 0, buffer.Length);
+                        }
                     }
                 }
-                else if (args[0] == "/c")
+            }
+            Console.WriteLine(string.Format("Finished writing data. Total size: {0} KiB", eof / 1024));
+            return 0;
+        }
+
+        static int ListBin(string source)
+        {
+            if (!File.Exists(source))
+            {
+                return 1;
+            }
+            Dictionary<string, long> folders = new Dictionary<string, long>();
+            using (Stream src = File.OpenRead(source))
+            {
+                byte[] buffer = new byte[64];
+                string filename = "";
+                int offset = 0;
+                long loc = 0;
+                long end_of_toc = 9999;
+                bool intoc = true;
+                List<byte> pointer = new List<byte> ();
+                bool insub = false;
+                string folder = "";
+                long folder_loc = 0;
+                while ((offset = src.Read(buffer, 0, buffer.Length)) > 0)
                 {
-                    Console.WriteLine("Repacking is not available in this version of Flipnic Game Data Extractor.");
-                    return 2;
-                }
-                else
-                {
-                    Console.WriteLine("The syntax of the requested command is incorrect.");
-                    return 3;
+                    byte[] cache = buffer;
+                    if (intoc)
+                    {
+                        filename = "";
+                        if (loc == end_of_toc)
+                        {
+                            intoc = false;
+                            continue;
+                        }
+                        pointer.Clear();
+                        foreach (byte b in cache[..60])
+                        {
+                            if (b == 0x00)
+                            {
+                                continue;
+                            }
+                            filename += Encoding.ASCII.GetString(new[] { b });
+                        }
+                        byte[] bytes = cache[60..];
+                        long byteoffset = (long)(BitConverter.ToInt32(bytes, 0)) * 2048;
+                        if (filename == "*Top Of CD Data")
+                        {
+                            end_of_toc = byteoffset;
+                            continue;
+                        }
+                        if (filename == "*End Of CD Data")
+                        {
+                            intoc = false;
+                            continue;
+                        }
+                        if (filename.EndsWith("\\"))
+                        {
+                            folders[filename] = byteoffset;
+                        }
+                        Console.WriteLine(string.Format("\\{0} (offset: 0x{1})", filename, byteoffset.ToString("X")));
+                    } else if (insub)
+                    {
+                        int i = cache.Length - 5;
+                        while (cache[i] == 0)
+                            --i;
+                        byte[] name = cache[..(i+1)];
+                        byte[] soff = cache[60..];
+                        filename = Encoding.ASCII.GetString(name);
+                        long byteoffset = (long)(BitConverter.ToUInt32(soff, 0)) + folder_loc;
+                        if (filename == "*End Of Mem Data")
+                        {
+                            insub = false;
+                        } else
+                        {
+                            Console.WriteLine(string.Format("\\{0}{1} (offset: 0x{2})", folder, filename, byteoffset.ToString("X")));
+                        }
+                    }
+                    else
+                    {
+                        foreach (KeyValuePair<string, long> kvp in folders)
+                        {
+                            if (kvp.Value == loc)
+                            {
+                                insub = true;
+                                folder = kvp.Key;
+                                folder_loc = kvp.Value;
+                                int i = cache.Length - 5;
+                                while (cache[i] == 0)
+                                    --i;
+                                byte[] name = cache[..(i + 1)];
+                                byte[] soff = cache[60..];
+                                filename = Encoding.ASCII.GetString(name);
+                                long byteoffset = (long)(BitConverter.ToUInt32(soff, 0)) + kvp.Value;
+                                Console.WriteLine(string.Format("\\{0}{1} (offset: 0x{2})", kvp.Key, filename, byteoffset.ToString("X")));
+                            }
+                        }
+                    }
+
+                    loc += 64;
                 }
             }
             return 0;
         }
-
         static int ExtractBin(string source, string destination)
         {
             if (Directory.Exists(destination))
--- README.md
+++ README.md
@@ -14,8 +14,21 @@ This tool allows you to extract data from .BIN files using a properietary format
 * Same filenames are allowed
 ## Command line usage
 * To extract data, use the following syntax: `FlipnicBinExtractor /e [source] [destination]`
-* Repacking is not available in this version (but is still in development)
+* To list directory, use the following syntax: `FlipnicBinExtractor /l [source]`
+* To data to a subfolder, use the following syntax: `FlipnicBinExtractor /f [source] [destination]`
+* Full BIN repacking is not available in this version (but is still in development)
 ## Examples
 * `FlipnicBinExtractor /e RES.BIN Extracted`
 * `FlipnicBinExtractor /e FONT.BIN FONT`
+* `FlipnicBinExtractor /l RES.BIN`
+* `FlipnicBinExtractor /f BOSS1 BOSS1\A`
 * `FlipnicBinExtractor /c FONT FONT.BIN` (not available in current version)
+## VGMToolBox settings
+These settings can be used in VGMToolBox (VGMToolbox > Misc. Tools > Extraction Tools > Generic > Virtual File System Extractor) for extracting BIN files (note that this will only work if the BIN file has no subfolders, you will need to use my tool for more advanced extraction purposes):
+* Header Size or File Count > Header Ends at Offset `<offset of the last "*End Of CD Data" file>`
+* File Record Information > File Records begin at offset `0` and each record has size `0x40`
+* Individual File Offset > File Offset is at Offset `60` and has size `4` and byte order `Little Endian`
+	* and do calculation (use $V to represent the value at the offset) `$V*2048`
+* Individual File Length > Use File Offsets to determine File Lengths
+* Individual File Name Location/Offset > File Name is Included in the Individual File Record at Offset `0`
+* Individual File Name Size > Has Static Size `59`
